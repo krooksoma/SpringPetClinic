c92a8902c3e806b189156a9ce97deb54ec1aed72
Romulo <romulojusto@gmail.com>
2021-09-27 09:32:55 -0400

    yet another commit

--- pet-clinic-data/src/test/java/SpringPetClinicApplicationTests.java
+++ pet-clinic-data/src/test/java/SpringPetClinicApplicationTests.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@ContextConfiguration
 class SpringPetClinicApplicationTests {
 
 	@Test
