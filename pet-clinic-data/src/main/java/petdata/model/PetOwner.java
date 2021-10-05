b9d18fb7bdc573a4bcaf6513fa20cce2f5fac5b2
Romulo <romulojusto@gmail.com>
2021-10-05 10:30:30 -0400

    closes #11

--- pet-clinic-data/src/main/java/petdata/model/PetOwner.java
+++ pet-clinic-data/src/main/java/petdata/model/PetOwner.java
@@ -1,4 +1,16 @@
 package petdata.model;
 
+import java.util.Set;
+
 public class PetOwner extends Person{
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
