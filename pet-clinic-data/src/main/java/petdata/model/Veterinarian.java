47f1853e9f01406cf8e821fe9a8cf6ead5d303d4
Romulo <romulojusto@gmail.com>
2021-10-05 10:35:32 -0400

    create veterinarian specialties that extends base entity. closes #15

--- pet-clinic-data/src/main/java/petdata/model/Veterinarian.java
+++ pet-clinic-data/src/main/java/petdata/model/Veterinarian.java
@@ -1,4 +1,16 @@
 package petdata.model;
 
+import java.util.Set;
+
 public class Veterinarian extends Person{
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
