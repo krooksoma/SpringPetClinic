acbba455eb26d33f44e70a1c4db3910eb47e74b0
Romulo <romulojusto@gmail.com>
2021-12-30 16:26:46 -0500

    Implemented base entity with JPA Annotation

--- pet-clinic-data/src/main/java/petdata/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/petdata/model/BaseEntity.java
@@ -1,18 +1,26 @@
 package petdata.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+// the following annotation establishes this class as a base class
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() {return id;}
 
     public void setId(Long id) {
         this.id = id;
     }
 
+    public boolean isNew(){
+        return this.id == null;
+    }
 
 }
